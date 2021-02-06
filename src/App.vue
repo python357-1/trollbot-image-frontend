3d550a3f91603e23207f158b2216a1dd57b73924
python357-1 <jb2101554@gmail.com>
2021-02-06 12:54:41 -0600

    functionality is done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
 	<h1>discord image maker</h1>
-	<form action="placeholder value">
+	<form action="https://python357-image-generator.herokuapp.com/create?" method='get'>
 		<input 
 			type="text"
 			name="username"
 			id="username"
 			placeholder='discord username'
 			>
+		<br>
+		<input type='radio' id='nickname' name='type' value='nickname'>
+		<label for="nickname">nickname</label>
+		<input type='radio' id='status' name='type' value='status'>
+		<label for="status">status</label>
+		<input type="radio" name="type" id="both" value='both'>
+		<label for="both">both</label>
 		<br>
 		<input type="submit" value="submit">
 	</form>
